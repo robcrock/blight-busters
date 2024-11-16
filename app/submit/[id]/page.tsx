5d2824dbf99f5e03b027ca0ce6aafa3b909c6fa6
JC <jcsmileyjr@gmail.com>
2024-11-15 23:12:38 -0600

    When the user take a picture, its displayed

--- app/submit/[id]/page.tsx
+++ app/submit/[id]/page.tsx
@@ -1,13 +1,29 @@
 'use client'
 import FileUpload from '../../images/upload-1.png';
 import Image from 'next/image';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 interface SubmitPageParams {
   id: string; // or number, depending on the type of id
 }
 
 function SubmitPage({ params }: { params: SubmitPageParams }) {
+  const fileInputRef = useRef<HTMLInputElement>(null); 
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => { 
+    const file = event.target.files && event.target.files[0]; 
+    if (file) { 
+      const reader = new FileReader(); 
+      reader.onload = (e) => { 
+        setImageSrc(e.target?.result as string); }; 
+        reader.readAsDataURL(file); 
+        console.log(imageSrc)
+        console.log("done")
+    }
+  };
+
+  
   
     return (
       <main className="flex min-h-screen flex-col p-8">
@@ -22,18 +38,15 @@ function SubmitPage({ params }: { params: SubmitPageParams }) {
           <div className="rounded-full bg-gray-200 flex items-center justify-center p-8 h-20 font-bold ">+25</div>
         </section>
         <section className="m-8">
-          {/* <div className='flex flex-col items-center justify-center'>
-            <Image src={FileUpload} alt="File Upload" />
-            <input title="Take a Picture" type="file" className="appearance-none cursor-pointer hidden" alt="Click to upload" />
-          </div> */}
-          <div className="flex items-center justify-center w-full">
+          <div className="flex flex-col items-center justify-center w-full">
             <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
                 <div className="flex flex-col items-center justify-center pt-5 pb-6">
                 <Image src={FileUpload} alt="File Upload" />
                     <p className="mb-2 text-sm text-gray-500 dark:text-gray-400 mt-2">Take a Picture</p>
                 </div>
-                <input id="dropzone-file" type="file" className="hidden" />
+                <input id="dropzone-file" type="file" width={20} className="hidden" ref={fileInputRef} onChange={handleFileChange} />
             </label>
+            {imageSrc && <Image src={imageSrc} alt="Uploaded Image" width={100} height={100} className='w-full mt-16'/>}
         </div> 
         </section>
       </main>
